ae24e0cbb47429faf816f6b328e745ce8ec16749
Julynx <julicabria@outlook.com>
2025-08-02 13:06:23 +0200

    bugfixes and style fixes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   maxCollectionSize: 1000,
   startingMoney: 20.00,
-  // freePackInterval: 6 * 60 * 60 * 1000
-  freePackInterval: 30000
+  freePackInterval: 6 * 60 * 60 * 1000
+  // freePackInterval: 30000  // For testing purposes
 };
