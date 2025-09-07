f2945398c69505fee6bf4b2095bb2e2d0e5ea3ae
Julynx <julicabria@outlook.com>
2025-09-07 21:05:13 +0200

    fix collection cap

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  maxCollectionSize: 1000,
+  maxCollectionSize: 1000000,
   startingMoney: 20.00,
   freePackInterval: 6 * 60 * 60 * 1000
   // freePackInterval: 30000  // For testing purposes
